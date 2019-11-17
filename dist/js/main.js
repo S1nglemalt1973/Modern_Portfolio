607e4342ff1f592cd63af8268cfebe15abe94d7e
Kim Wood <kimwood@Kims-MacBook.local>
2019-11-17 09:51:03 -0500

    Menu button, javascript & rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,7 +18,7 @@ function toggleMenu() {
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
-    //Set Menu Stte
+    //Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
@@ -27,7 +27,7 @@ function toggleMenu() {
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
-    //Set Menu Stte
+    //Set Menu State
     showMenu = false;
   }
 }
